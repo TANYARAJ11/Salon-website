4e45d06f389fac27a9fb6ba495ada28589c79c2b
Tanya Raj <73035345+TANYARAJ11@users.noreply.github.com>
2022-06-23 00:56:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+const linkColor = document.querySelectorAll('.nav-link')
+function colorLink(){
+    if(linkColor){
+        linkColor.forEach(L => L.classList.remove('active'))
+        this.classList.add('active')
+    }
+}
+linkColor.forEach(L => L.addEventListener('click', colorLink))
+
+function validation(){
+    alert("Thanks for signing in");
+}
+function checkout(){
+    alert("You have been checked out successfully");
+}
